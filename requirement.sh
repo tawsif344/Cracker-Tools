a027b8abd093c9c805525473ca9ff3ef73178fad
tawsif <84251628+tawsif344@users.noreply.github.com>
2022-07-18 22:41:03 +0600

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,17 @@
+pkg install figlet -y 
+pip install requests
+pip install mechanize
+
+# remove play-store coution notice:
+rm -rf /data/data/com.termux/files/usr/etc/motd-playstore
+
+pip install pyfiglet
+pip install bs4
+pip install lolcat
+pip install cracker-say
+pip install cowsay 
+pip install youtube_dl
+pip install gTTS
+pip install telethon
+pip install python-telegram-bot
+pip install pyshorteners
